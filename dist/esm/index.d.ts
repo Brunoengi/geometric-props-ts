27ec980ba55f5f08693c95b8c90ad27fe23de10c
Bruno <bruno.if@hotmail.com>
2024-07-15 03:25:03 -0300

    add support types

---
+++ dist/esm/index.d.ts
@@ -0,0 +1,2 @@
+export { GeometricProps } from './GeometricProps.js';
+export * from './GeometricProps.js';
---
+++ dist/esm/interfaces/IGeometricProps.d.ts
@@ -0,0 +1,50 @@
+export interface IGeometricProps {
+    A: number;
+    Sx: number;
+    Sy: number;
+    Ix: number;
+    Iy: number;
+    Ixy: number;
+    Ixg: number;
+    Iyg: number;
+    Ixyg: number;
+    Y1: number;
+    Y2: number;
+    W1: number;
+    W2: number;
+    Xmax: number;
+    Ymax: number;
+    Xmin: number;
+    Ymin: number;
+    Xg: number;
+    Yg: number;
+    height: number;
+    base: number;
+}
+export interface IEquivalentFigures {
+    A: number;
+    Sx: number;
+    Sy: number;
+    Ix: number;
+    Iy: number;
+    Ixy: number;
+    Ixg: number;
+    Iyg: number;
+    Ixyg: number;
+    Y1: number;
+    Y2: number;
+    W1: number;
+    W2: number;
+}
+export interface IBidimensionalPoint {
+    x: number;
+    y: number;
+}
+export interface IGreenTheoremLine {
+    x0: number;
+    x1: number;
+    y0: number;
+    y1: number;
+    dx: number;
+    dy: number;
+}
