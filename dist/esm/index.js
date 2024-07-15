81925fad344b6f5abb0ae1b2cca34a5dfb17b078
Bruno <bruno.if@hotmail.com>
2024-07-15 00:51:55 -0300

    support to esm and cjs

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,5 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GeometricProps = void 0;
-var GeometricProps_js_1 = require("./GeometricProps.js");
-Object.defineProperty(exports, "GeometricProps", { enumerable: true, get: function () { return GeometricProps_js_1.GeometricProps; } });
+export { GeometricProps } from './GeometricProps.js';
